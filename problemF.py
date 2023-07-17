d34f9793c8088ddcecc721882b35e0edd78fcb45
Rawan Amr Abdelsattar Abdelaal <64783197+rawan-amr-abdelsattar1552006@users.noreply.github.com>
2023-07-17 16:56:26 +0300

    Add files via upload

--- problemF.py
+++ problemF.py
@@ -5,10 +5,9 @@ for i in range(n):
     smurf = input().count("1")
     smurfs.append(smurf)
 
-if len(list(set(smurfs))) == 1 and n % 2 == 0:
-    print(n)
-elif len(list(set(smurfs))) == 1 and n % 2 != 0:
-    print(n - 1)
+if len(list(set(smurfs))) == 1:
+    print(2)
+    
 else:
     win = smurfs.count(max(smurfs))
     print(win)
