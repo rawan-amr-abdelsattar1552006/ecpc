dff9a8fae95b8698bd0d30cd098ccb4bd4de47e7
Rawan Amr Abdelsattar Abdelaal <64783197+rawan-amr-abdelsattar1552006@users.noreply.github.com>
2023-07-17 16:20:14 +0300

    Update ProblemD.py

--- ProblemD.py
+++ ProblemD.py
@@ -1,43 +1,10 @@
-"""n = int(input())
-cont = [int(x) for x in input().split(" ")]
-
-count = 0
-for index,x in enumerate(cont):
-    if index == x:
-        count += 1
-print(count)
-"""
-"""from time import process_time
-n = int(input())
-nums = [int(x) for x in input().split(" ")]
-pairs = []
-for i in range(1,n + 1):
-    pairs.append((nums[i-1],i))
-
-count = 0
-for i in pairs:
-    for j in pairs:
-        if i[0] == j[1] and i[1] == j[0]:
-            count += 1
-            pairs.remove(i)
-            pairs.remove(j)
-print(count)
-print(process_time())
-"""
-
-
 n = int(input())
 nums = [int(x) for x in input().split(" ")]
 
 count = 0
-pairs = {}
 
 for i in nums:
-    pairs[i] = nums.index(i) + 1
-
-for i in pairs:
-    if i == pairs[pairs[i]]:
+    if nums[i - 1] == nums.index(i) + 1:
         count += 1
         
 print(count // 2)
-
